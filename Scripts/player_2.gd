2c0d862534083858aeb8be4eeab72626b5d500c4
rahmatfx <rahmatarifjoe111@gmail.com>
2026-01-30 21:11:27 +0700

    penambahan movement

--- Scripts/player_2.gd
+++ Scripts/player_2.gd
@@ -11,12 +11,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump_1") and is_on_floor():
+	if Input.is_action_just_pressed("Jump_2") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left_1", "Right_1")
+	var direction := Input.get_axis("Left_2", "Right_2")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
