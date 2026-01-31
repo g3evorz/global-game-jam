9ccf3ace1d4d79eb0ce6ac6d043d20767166ec37
rahmatfx <rahmatarifjoe111@gmail.com>
2026-01-31 16:30:56 +0700

    kenapaaaa

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,13 +1,20 @@
 extends Node
+@onready var timer = $"../CanvasLayer/ControlUI/VBoxContainer/ControlTimer/Label"
+@onready var level_timer = $"../Timer"
 
 
+func  game_over() :
+	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	timer.text = str(int(level_timer.time_left))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-func  game_over() :
-	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
+	timer.text = str(int(level_timer.time_left))
+
+
+
+func _on_timer_timeout() -> void:
+	game_over()
