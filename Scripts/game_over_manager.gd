052334064f5163a9c68dd661650d040dc41fe7cd
Alfan Alauddin <130551799+g3evorz@users.noreply.github.com>
2026-01-31 11:11:59 +0700

    Revert "game_over"

--- Scripts/game_over_manager.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func reset():
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-
-func _on_retry_button_pressed() -> void:
-	reset()
--- Scripts/kill_zone.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-func  game_over() :
-	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
-	
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player_1" or "Player_2":
-		game_over()
