ad6b772bd5f36855d8ad92f9e9d55fae76db30ad
rahmatfx <rahmatarifjoe111@gmail.com>
2026-01-31 17:14:46 +0700

    rawd

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,19 +1,34 @@
 extends Node
+<<<<<<< Updated upstream
 @onready var timer = $"../CanvasLayer/ControlUI/VBoxContainer/ControlTimer/Label"
 @onready var level_timer = $"../Timer"
+=======
+@onready var timer = $"../CanvasLayer/ControlUI/VBoxContainer/TimerControl/Countdown"
+@onready var timer_level = $"../Timer"
+>>>>>>> Stashed changes
 
+func  game_over() :
+	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
 
 func  game_over() :
 	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+<<<<<<< Updated upstream
 	timer.text = str(int(level_timer.time_left))
+=======
+	timer.text = str(int(timer_level.time_left))
+>>>>>>> Stashed changes
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+<<<<<<< Updated upstream
 	timer.text = str(int(level_timer.time_left))
 
+=======
+	timer.text = str(int(timer_level.time_left))
+>>>>>>> Stashed changes
 
 
 func _on_timer_timeout() -> void:
