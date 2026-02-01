67ae5630a134612db6783e298a847eafc3ac5b95
rahmatfx <rahmatarifjoe111@gmail.com>
2026-02-01 11:11:24 +0700

    pause

---
+++ Scenes/pause_menu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+func _ready():
+	visible = false
+
+func _input(event):
+	if event.is_action_pressed("ui_pause") and !event.is_echo():
+		if get_tree().paused:
+			resume_game()
+		else:
+			pause_game()
+
+func pause_game():
+	visible = true
+	get_tree().paused = true
+
+func resume_game():
+	visible = false
+	get_tree().paused = false
+
+
+func _on_resume_button_pressed() -> void:
+	resume_game()
+
+
+func _on_back_to_main_menu_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/Homescreen.tscn")
